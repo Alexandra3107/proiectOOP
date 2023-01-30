7c3ad98c1c51f7b04e27162b8a3b63334f946f1c
ale.Alexandra <alexandra.n3107@gmail.com>
2023-01-30 21:28:32 +0200

    Primul commit

---
+++ src/ElonsToyCar.java
@@ -0,0 +1,43 @@
+public class ElonsToyCar {
+    private int metersDriven;
+    private int batteryPercentage;
+
+    public static void main(String[] args) {
+        ElonsToyCar car1 = ElonsToyCar.buy();
+        System.out.println(car1);
+        System.out.println(car1.distanceDisplay());
+        System.out.println(car1.batteryDisplay());
+        for(int i=0; i<98; i++){
+            car1.drive();
+        }
+        System.out.println(car1.distanceDisplay());
+        System.out.println(car1.batteryDisplay());
+
+    }
+    public static ElonsToyCar buy() {
+        return new ElonsToyCar();
+    }
+    private ElonsToyCar() {
+        this.metersDriven = 0;
+        this.batteryPercentage = 100;
+    }
+
+    public String distanceDisplay() {
+        return "Driven " + this.metersDriven + " meters";
+    }
+
+    public String batteryDisplay() {
+        if (this.batteryPercentage == 0) {
+            return "Battery empty";
+        }
+        return "Battery at " + this.batteryPercentage + "%";
+    }
+
+    public void drive() {
+        if (this.batteryPercentage == 0) {
+            return;
+        }
+        this.metersDriven += 20;
+        this.batteryPercentage--;
+    }
+}
---
+++ src/NeedForSpeed.java
@@ -0,0 +1,69 @@
+class NeedForSpeed {
+    int speed;
+    int batteryDrain;
+    int distanceDriven = 0;
+    int batteryLeft = 100;
+
+    public static void main(String[] args) {
+        NeedForSpeed car = new NeedForSpeed(5, 2);
+        RaceTrack raceTrack = new RaceTrack(10);
+        System.out.println(raceTrack.distance);
+        car.drive();
+        car.drive();
+        System.out.println(car.batteryLeft);
+        System.out.println(car.distanceDriven);
+        System.out.println(car.batteryLeft);
+        System.out.println(car.batteryDrained());
+        NeedForSpeed nitroCar = NeedForSpeed.nitro();
+        nitroCar.drive();
+        nitroCar.drive();
+        nitroCar.drive();
+        System.out.println(nitroCar.distanceDriven);
+        System.out.println(nitroCar.batteryLeft);
+        System.out.println(raceTrack.carCanFinish(car));
+    }
+
+    NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+    }
+
+    public void drive() {
+        if (batteryLeft < batteryDrain) {
+            return;
+        }
+        distanceDriven += speed;
+        batteryLeft -= batteryDrain;
+    }
+
+    int distanceDriven() {
+        return distanceDriven;
+    }
+
+    boolean batteryDrained() {
+        return batteryLeft < batteryDrain;
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+}
+
+class RaceTrack {
+    int distance;
+
+    RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    boolean carCanFinish(NeedForSpeed car) {
+        while (!car.batteryDrained()) {
+            car.drive();
+            if (car.distanceDriven() >= distance) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
